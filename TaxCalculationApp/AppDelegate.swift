7bebf5bc8b74862a18f7df8c7c20f588a9ebdd07
Syuri-var <sayuri.yamamoto.555@gmail.com>
2023-09-05 00:39:19 -0700

    消費税額だけ反映させるところまでは行けましたが合計金額が表示できていません。エラーでコードの中にないファイルが有るようです。

--- TaxCalculationApp/AppDelegate.swift
+++ TaxCalculationApp/AppDelegate.swift
@@ -7,30 +7,47 @@
 
 import UIKit
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class ViewController: UIViewController {
+ 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    let taxRates: [Double] = [0.08, 0.10]
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
+        let selectedIndex = sender.selectedSegmentIndex
+        let selectedRate = taxRates[selectedIndex]
+        
+        // ここで消費税を計算（例えば、100円を想定）
+        let price = 100.0
+        let tax = price * selectedRate
+        let totalPrice = price + tax
+        
+        Label.text = "消費税: \(tax) 円\n合計: \(totalPrice) 円"
     }
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        // UITextFieldからテキストを取得し、計算を行い、結果を表示する
+        if let text = textField.text, let amount = Double(text) {
+            let selectedIndex = segmentedControl.selectedSegmentIndex
+            let selectedRate = taxRates[selectedIndex]
 
-    // MARK: UISceneSession Lifecycle
+            // 消費税を計算
+            let tax = amount * selectedRate
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+            // 合計金額を計算
+            let totalPrice = amount + tax
+
+            // 結果を表示用のラベルに表示
+            Label.text = "消費税: \(tax) 円\n合計: \(totalPrice) 円"
+        } else {
+            // テキストが数値に変換できない場合のエラーハンドリング
+            Label.text = "有効な金額を入力してください"
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-
 
 }
-
