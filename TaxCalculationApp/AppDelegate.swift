ad52e78a36056258f9ed745d335d39cfc426bb53
Syuri-var <sayuri.yamamoto.555@gmail.com>
2023-09-05 19:07:46 -0700

    over rideをおくべき位置がわからなくなってきました。クラス内においているのにどうしてエラーが出るのでしょうか。

--- TaxCalculationApp/AppDelegate.swift
+++ TaxCalculationApp/AppDelegate.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
+    
     @IBOutlet weak var Label: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var textField: UITextField!
-    
+    @IBOutlet weak var resultLabel: UILabel!
     
     let taxRates: [Double] = [0.08, 0.10]
-
+    
     @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
         let selectedIndex = sender.selectedSegmentIndex
         let selectedRate = taxRates[selectedIndex]
@@ -28,26 +28,27 @@ class ViewController: UIViewController {
         Label.text = "消費税: \(tax) 円\n合計: \(totalPrice) 円"
     }
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
-        // UITextFieldからテキストを取得し、計算を行い、結果を表示する
-        if let text = textField.text, let amount = Double(text) {
-            let selectedIndex = segmentedControl.selectedSegmentIndex
-            let selectedRate = taxRates[selectedIndex]
-
-            // 消費税を計算
-            let tax = amount * selectedRate
-
-            // 合計金額を計算
-            let totalPrice = amount + tax
-
-            // 結果を表示用のラベルに表示
-            Label.text = "消費税: \(tax) 円\n合計: \(totalPrice) 円"
-        } else {
-            // テキストが数値に変換できない場合のエラーハンドリング
-            Label.text = "有効な金額を入力してください"
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            calculateAndDisplayTotal()
+        }
+        func calculateAndDisplayTotal() {
+            if let text = textField.text, let amount = Double(text) {
+                let selectedIndex = segmentedControl.selectedSegmentIndex
+                let selectedRate = taxRates[selectedIndex]
+                
+                // 消費税を計算
+                let tax = amount * selectedRate
+                
+                // 合計金額を計算
+                let totalPrice = amount + tax
+                
+                // 結果を表示用のラベルに表示
+                resultLabel.text = "消費税込み価格: \(totalPrice) 円"
+            } else {
+                resultLabel.text = "金額を入力してください"
+            }
         }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
 }
--- TaxCalculationApp/ViewController.swift
+++ TaxCalculationApp/ViewController.swift
@@ -1,52 +1,40 @@
 //
-//  ViewController.swift
+//  AppDelegate.swift
 //  TaxCalculationApp
 //
 //  Created by Apple on 2023/09/04.
 //
+
 import UIKit
 
-class viewController: UIViewController {
- 
-    @IBOutlet weak var Label: UILabel!
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var textField: UITextField!
-    
-    
-    let taxRates: [Double] = [0.08, 0.10]
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
-        let selectedIndex = sender.selectedSegmentIndex
-        let selectedRate = taxRates[selectedIndex]
-        
-        // ここで消費税を計算（例えば、100円を想定）
-        let price = 100.0
-        let tax = price * selectedRate
-        let totalPrice = price + tax
-        
-        Label.text = "消費税: \(tax) 円\n合計: \(totalPrice) 円"
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
     }
-    @IBAction func calculateButtonTapped(_ sender: UIButton) {
-        // UITextFieldからテキストを取得し、計算を行い、結果を表示する
-        if let text = textField.text, let amount = Double(text) {
-            let selectedIndex = segmentedControl.selectedSegmentIndex
-            let selectedRate = taxRates[selectedIndex]
 
-            // 消費税を計算
-            let tax = amount * selectedRate
+    // MARK: UISceneSession Lifecycle
 
-            // 合計金額を計算
-            let totalPrice = amount + tax
-
-            // 結果を表示用のラベルに表示
-            Label.text = "消費税: \(tax) 円\n合計: \(totalPrice) 円"
-        } else {
-            // テキストが数値に変換できない場合のエラーハンドリング
-            Label.text = "有効な金額を入力してください"
-        }
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+
 
 }
+
+
+
+
+
